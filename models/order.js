9a6b73d6dfb95815beb8316e6b871188e1e1a253
nghiavu02 <nghiavu.dev@gmail.com>
2024-06-23 01:41:42 +0700

    upload code server

---
+++ models/order.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose'); // Erase if already required
+
+// Declare the Schema of the Mongo model
+var orderSchema = new mongoose.Schema({
+  products: [{
+    product: { type: mongoose.Types.ObjectId, ref: 'Product' },
+    quantity: Number,
+    color: String,
+    price: Number,
+    thumbnail: String,
+    title: String
+  }],
+  status: {
+    type: String,
+    default: 'Processing',
+    enum: ['Cancelled', 'Succeed','Processing' ]
+  },
+  total: Number,
+  orderBy: {
+    type: mongoose.Types.ObjectId,
+    ref: 'User',
+  },
+}, {
+  timestamps: true
+});
+
+//Export the model
+module.exports = mongoose.model('Order', orderSchema);
