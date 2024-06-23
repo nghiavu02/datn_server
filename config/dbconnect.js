bc25feee975ee0450b1d5803d254076983816548
Vũ Ngọc Nghĩa <135939350+nghiavu02@users.noreply.github.com>
2024-06-23 17:20:27 +0700

    Update dbconnect.js

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -3,7 +3,7 @@ const { default: mongoose } = require("mongoose");
 const dbConnect = async () => {
   try {
     const conn = await mongoose.connect(
-      "mongodb+srv://vnnghia02:Nghia.2002@cluster0.n6wzqm5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      "mongodb+srv://vnnghia02:Nghia.2002@cluster0.n6wzqm5.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0"
       // "mongodb://localhost:27017/ecommerce1"
     );
     if (conn.connection.readyState === 1)
